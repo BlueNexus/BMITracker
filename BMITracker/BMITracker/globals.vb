dd5200b1c634b2dd2b3a83a258da386f7acdf959
Unknown <benreeve99@yahoo.co.uk>
2018-04-19 11:54:02 +0100

    globals

---
+++ BMITracker/BMITracker/globals.vb
@@ -0,0 +1,22 @@
+﻿Imports Microsoft.VisualBasic
+
+Public Class dbfunc
+
+    Public Shared Function add_to_table(ByRef target As String, ByVal weight As Integer, ByVal bmi As Integer)
+        Dim success = True
+        Try
+            Using cn As New OleDb.OleDbConnection With {.ConnectionString = target}
+                Using cmd As New OleDb.OleDbCommand With {.Connection = cn}
+                    cmd.CommandText = "INSERT INTO Table1 (Weight, BMI) VALUES (@weight, @3);"
+                    cmd.Parameters.AddWithValue("@weight", weight)
+                    cmd.Parameters.AddWithValue("@3", bmi)
+                    cn.Open()
+                    cmd.ExecuteNonQuery()
+                End Using
+            End Using
+        Catch ex As Exception
+            success = False
+        End Try
+        Return success
+    End Function
+End Class
